ee8d0a179ea31e92c0c807746244dddfd1c26b8d
Gustavo Orth <gustavogauerorth@gmail.com>
2024-01-30 16:12:05 -0300

    rename

---
+++ Jogos/termo/palavras.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <button onclick="funcao('excluir')" class="btn btn-danger" type="button">Excluir</button>
+    <button onclick="funcao()" class="btn btn-success" type="button">Manter</button>
+    <div id="eu"></div>
+<script src="js.js"></script>
+</body>
+</html>
